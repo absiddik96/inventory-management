6855f2b69602c7e5b5c5b61333934e022c0cdf71
kawsar <hmkawsar559@gmail.com>
2019-03-05 12:13:38 +0600

    UsersTableSeeder data included

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        factory(App\User::class)->create();
+        $this->call(UsersTableSeeder::class);
+        // factory(App\User::class)->create();
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(array(
+
+            'name'    => 'Admin',
+            'email'         => 'admin@gmail.com',
+            'password'      => bcrypt('123456'), //hashes our password nicely for us
+            'is_admin'  => 1,
+
+        ));
+
+        User::create(array(
+
+            'name'    => 'Regular User',
+            'email'         => 'user@gmail.com',
+            'password'      => bcrypt('123456'), //hashes our password nicely for us
+            'is_admin'  => 0,
+
+        ));
+    }
+}
